bb905fedcda0423738721eb537d00faf4cb8055b
TheRandomLabs <therandomlabsinc@gmail.com>
2021-01-03 10:57:51 +1100

    Minor cleanup

--- src/main/java/com/therandomlabs/autoconfigtoml/TOMLConfigSerializer.java
+++ src/main/java/com/therandomlabs/autoconfigtoml/TOMLConfigSerializer.java
@@ -226,11 +226,14 @@ public final class TOMLConfigSerializer<T extends ConfigData> implements ConfigS
 		try {
 			final File file = fileConfig.getFile();
 
-			try (final BOMInputStream stream = new BOMInputStream(new FileInputStream(file))) {
+			try (BOMInputStream stream = new BOMInputStream(new FileInputStream(file))) {
 				if (stream.hasBOM()) {
 					//https://github.com/TheElectronWill/night-config/issues/67
 					//This only seems to be a problem with LF line endings...
-					FileUtils.write(file, IOUtils.toString(stream, StandardCharsets.UTF_8));
+					FileUtils.write(
+							file, IOUtils.toString(stream, StandardCharsets.UTF_8),
+							StandardCharsets.UTF_8
+					);
 				}
 
 				fileConfig.load();
